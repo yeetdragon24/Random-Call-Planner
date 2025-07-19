63c5b18e3838da8bb240cbb040d98643e4e9cb01
YeetDragon23 <100045569+yeetdragon24@users.noreply.github.com>
2025-07-19 14:23:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ app.controller('myCtrl', function ($scope) {
 			if ($scope.mode.id == 'grimoire') currentSpell += $scope.spellsCastTotal;
 
             if ($scope.render) {
-                lookaheadList.appendChild(createListItem(`${$scope.spellsCastThisAscension + i + 1} | ${$scope.spellsCastTotal + $scope.spellsCastThisAscension + i + 1}`, true));
+                lookaheadList.appendChild(createListItem(`${$scope.spellsCastThisAscension + i + 1} | ${$scope.spellsCastTotal + i + 1}`, true));
                 lookaheadList.appendChild(document.createElement('md-divider'));
             }
 
